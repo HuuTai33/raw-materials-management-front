1e74fce4cc1c091806acc30e31ed768570a03fb4
HuuTai Tran <htran@lm0019.home>
2023-07-11 03:46:02 +0200

    add register page

--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {UserService} from "../../core/api/user/user.service";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
+  email: string = '';
+  password: string = '';
+  errorText: string = '';
+
+  constructor(private router: Router,
+              private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  redirectConnect() {
+    this.router.navigateByUrl('/home');
+  }
+
+  register() {
+    if (this.email != '' && this.password != '') {
+      this.userService.createUser({email: this.email, password: this.password}).subscribe( result => {
+        this.errorText = '';
+        this.router.navigateByUrl('/home');
+      }, error => {
+        if (error.status === 422) {
+          this.errorText = 'Identifiants incorrects ou email déjà utilisé.'
+        }
+      })
+    }
+  }
 }
