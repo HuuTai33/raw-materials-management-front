451673d74998021a43073093e149e34dcae9dadb
HuuTai Tran <htran@lm0019.home>
2023-07-16 11:46:20 +0200

    add Edit page

--- src/app/features/raw-material/raw-material-detail/raw-material-detail.component.ts
+++ src/app/features/raw-material/raw-material-detail/raw-material-detail.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {RawMaterialService} from "../../../core/api/raw-material/raw-material.service";
 
 @Component({
   selector: 'app-raw-material-detail',
   templateUrl: './raw-material-detail.component.html',
   styleUrls: ['./raw-material-detail.component.scss']
 })
-export class RawMaterialDetailComponent {
+export class RawMaterialDetailComponent implements OnInit {
+
+  rawmaterial: any
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private rawmaterialService: RawMaterialService) {
+  }
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.rawmaterialService.getRawMaterial(Number(id)).subscribe(result => {
+      this.rawmaterial = result;
+    }, error => {
+      this.router.navigateByUrl('/rawmaterials');
+    })
+  }
+
+  edit() {
+    this.router.navigate(['/rawmaterials/' + this.rawmaterial.id + '/edit']);
+  }
+
+  delete() {
+    this.rawmaterialService.deleteRawMaterial(this.rawmaterial.id).subscribe(result => {
+      this.router.navigateByUrl('/rawmaterials');
+    })
+  }
 
 }
