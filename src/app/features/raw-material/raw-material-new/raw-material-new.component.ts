eaacf238ed96282cbace470dfd576b94012b2626
HuuTai Tran <htran@lm0019.home>
2023-07-16 11:25:38 +0200

    add new rawmaterials page

--- src/app/features/raw-material/raw-material-new/raw-material-new.component.ts
+++ src/app/features/raw-material/raw-material-new/raw-material-new.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {RawMaterialService} from "../../../core/api/raw-material/raw-material.service";
 
 @Component({
   selector: 'app-raw-material-new',
   templateUrl: './raw-material-new.component.html',
   styleUrls: ['./raw-material-new.component.scss']
 })
-export class RawMaterialNewComponent {
+export class RawMaterialNewComponent implements OnInit {
+
+  name: string = '';
+  category: string = '';
+  supplier: string = '';
+
+  constructor(private router: Router,
+              private rawmaterialService: RawMaterialService) {
+  }
+
+  ngOnInit(): void {
+    //TODO : Get session to check if connected
+  }
+
+  cancel() {
+    this.router.navigateByUrl('/rawmaterials');
+  }
+
+  create() {
+    const params = {
+      name: this.name,
+      category: this.category,
+      supplier: this.supplier,
+      score: 0
+    }
+    this.rawmaterialService.createRawMaterial(params).subscribe(result => {
+      this.router.navigateByUrl('/rawmaterials');
+    })
+  }
 
 }
